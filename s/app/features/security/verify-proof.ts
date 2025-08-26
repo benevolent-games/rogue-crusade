26639da8cafbea0f73975e140c2c71270273b945
Chase Moskal <chasemoskal@gmail.com>
2025-08-26 15:31:12 -0700

    wip: assets and token stuff

--- s/app/features/security/verify-proof.ts
+++ s/app/features/security/verify-proof.ts
@@ -3,8 +3,8 @@ import {Proof} from "@authlocal/authlocal"
 
 export async function verifyProof(proofToken: string) {
 	return Proof.verify(proofToken, {allowedAudiences: [
-		window.location.origin,
 		"http://localhost:8080",
+		"http://localhost:8081",
 		"https://rogue.benevolent.games",
 	]})
 }
